e30f1aeb65683215d5d249b17195995a54921b8f
Takanobu <61861946+takanobu68@users.noreply.github.com>
2023-01-10 15:09:31 +0900

    Twete

--- pages/api/item/delete/[id].js
+++ pages/api/item/delete/[id].js
@@ -1,6 +1,6 @@
 import connectDB from '../../../../utils/database';
 import { ItemModel } from '../../../../utils/schemaModels';
-import auth from '../../../../utils';
+import auth from '../../../../utils/auth';
 
 const deleteItems = async (req, res) => {
   try {
